79334487b1076ae088c632c4a9a9077949a2ca04
Jonathan Damiani <jdamiani@Jonathans-MacBook-Pro.local>
2022-04-17 12:29:22 -0700

    removing redux and cleaning up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/globals.css'
-import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
-import { store } from '../app/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
