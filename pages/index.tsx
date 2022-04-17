32ed7cadcc24656a0669885948854d7f6b3909b9
Jonathan Damiani <jdeandamianiapps@gmail.com>
2022-04-17 16:58:52 -0700

    adding contentful with localization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
-import type { NextPage } from 'next'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import { IMainFields } from '../src/@types/contentful'
+import ContentService from '../src/utils/content-service'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+interface Props {
+  main: IMainFields
+}
+
+const Home: NextPage<Props> = ({ main }) => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Jonathan Damiani</title>
-        <meta
-          name="description"
-          content="Software Engineer Frontend Portfolio"
-        />
+        <meta name="description" content="Frontend Engineer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div>{documentToReactComponents(main.description!)}</div>
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>
@@ -22,3 +28,16 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
+  const main = await ContentService.instance.getEntryById<IMainFields>(
+    process.env.CONTENTFUL_MAIN_ID,
+    locale!
+  )
+
+  return {
+    props: {
+      main: main.fields,
+    },
+  }
+}
